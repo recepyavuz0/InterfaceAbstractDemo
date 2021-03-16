5197fa8cbe43715b500cec31e20b307d47ca032a
caferyavuz <caferyavuz134@gmail.com>
2021-03-16 21:26:34 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Concreate;
+using InterfaceAbstractDemo.Entities;
+using InterfaceAbstractDemo.Adapters;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager baseCustomerManager = new NeroCustomerManager(new MernisServiceAdapter());
+            
+            // Hatalı Kullanıcı Bilgileri Yollandı.
+            baseCustomerManager.Save(new Customer() { DateOfBirth = new DateTime(1986,8,19),FirstName="Ali",LastName="Cab",Id=1, NationalityId="11111111111"}); ;
+            
+        }
+    }
+}
